9d0689fa8a23f9e401d7ef7265a17474afdf22fd
Amanuel Bekele <amanuelbekele32@gmail.com>
2024-08-08 14:21:59 -0500

    Add prod configurations for Replit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const readline = require('readline');
+const http = require('http');
 const processCommand = require('./app');
 const Logger = require('./utils/logger');
 
@@ -16,5 +17,15 @@ rl.on('line', (input) => {
 logger.log('File System Simulator');
 logger.log('Enter commands (CREATE, LIST, DELETE, MOVE). Only one command at a time.');
 
-// Keep the program running
-setInterval(() => {}, 1000);
+// Create HTTP server - this is just to keep the program running on Replit
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('File System Simulator\n  Enter commands (CREATE, LIST, DELETE, MOVE) in the console below.');
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  logger.log(`Server running on port ${port}`);
+});
+
